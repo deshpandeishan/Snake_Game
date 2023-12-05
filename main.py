ae712a83bff17cbad72f40041aad6b3b6de9e4cc
Ishan Deshpande <deshpandeishan75@gmail.com>
2023-12-05 20:45:30 +0530

    Added direction changing feature.

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
+
 screen = Screen()
 screen.setup(600, 600)
 screen.title("Snake Game")
 screen.bgcolor("black")
+screen.tracer(0)
 
+python = Snake()
 
-segments_list = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
+screen.listen()
+screen.onkey(python.up, "Up")
+screen.onkey(python.down, "Down")
+screen.onkey(python.left, "Left")
+screen.onkey(python.right, "Right")
 
-for position in segments_list:
-    new_segment = Turtle("square")
-    new_segment.penup()
-    new_segment.color("white")
-    new_segment.goto(position)
-    screen.tracer(0)
-    segments.append(new_segment)
 
 game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
-    for segment_number in range(len(segments) - 1, 0, -1):
-        x = segments[segment_number - 1].xcor()
-        y = segments[segment_number - 1].ycor()
-        segments[segment_number].goto(x, y)
-    segments[0].fd(20)
-    segments[0].lt(90)
-
-
+    python.move()
 
 screen.exitonclick()
