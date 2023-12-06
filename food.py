f8b763121762ba1c62977219aa546591f894ecc2
Ishan Deshpande <deshpandeishan75@gmail.com>
2023-12-06 19:44:18 +0530

    Fixed the grain bug.

--- food.py
+++ food.py
@@ -7,12 +7,12 @@ grain = Turtle()
 class Food(Turtle):
     def __init__(self):
         super().__init__()
-        grain.shape("circle")
-        grain.penup()
-        grain.color('green')
-        grain.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        grain.speed("fastest")
+        self.shape("circle")
+        self.penup()
+        self.color('green')
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.speed("fastest")
         self.refresh_location()
 
     def refresh_location(self):
-        self.goto(randint(-300, 300), randint(-300, 300))
+        self.goto(randint(-280, 280), randint(-280, 280))
--- main.py
+++ main.py
@@ -9,22 +9,22 @@ screen.title("Snake Game")
 screen.bgcolor("black")
 screen.tracer(0)
 
-python = Snake()
+snake = Snake()
 food = Food()
 
 screen.listen()
-screen.onkey(python.up, "Up")
-screen.onkey(python.down, "Down")
-screen.onkey(python.left, "Left")
-screen.onkey(python.right, "Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
-    python.move()
+    snake.move()
 
-    if python.head.distance(food) < 15:
-        food.refresh_location()  # Facing bug in this line.
+    if snake.head.distance(food) < 15:
+        food.refresh_location()
 
 screen.exitonclick()
