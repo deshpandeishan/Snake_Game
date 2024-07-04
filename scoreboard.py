f167d78f2d5be19bca1c8df88912c9c4da79b09c
Ishan Deshpande <deshpandeishan75@gmail.com>
2024-07-04 10:44:24 +0530

    Add address to store highscore

--- scoreboard.py
+++ scoreboard.py
@@ -4,6 +4,7 @@ from turtle import Turtle
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        self.high_score = 0
         self.score = 0
         self.color("white")
         self.penup()
@@ -17,7 +18,14 @@ class Scoreboard(Turtle):
         self.update_scores()
 
     def update_scores(self):
-        self.write(f"Score: {self.score}", align='center', font=('Courier', 27, 'normal'))
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align='center', font=('Courier', 27, 'normal'))
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scores()
 
     def game_over(self):
         self.goto(0, 0)
